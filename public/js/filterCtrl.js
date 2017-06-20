207963a9e61b22be0290081e0b6b982d6cff1530
CarolinaRapetti <scrapetti@outlook.com>
2017-06-20 09:18:52 -0300

    Correccion de algunos errores de dependencias

---
+++ public/js/filterCtrl.js
@@ -0,0 +1,13 @@
+var filterCtrl = angular.module('filterCtrl', []);
+filterCtrl.controller('filterCtrl', function($scope, $http){
+// Initializes Variables
+    // ----------------------------------------------------------------------------
+    $scope.formData = {};
+   
+    // Functions
+    // ----------------------------------------------------------------------------
+    // Creates a new heladeria based on the form fields
+    $scope.filtrarHeladerias = function() {
+
+    };
+});
