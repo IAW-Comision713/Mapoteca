6979cd787f861a1271ae88554525af08628f7a5b
CarolinaRapetti <scrapetti@outlook.com>
2017-06-29 15:30:19 -0300

    Distancia mas o menos

--- public/js/indexApp.js
+++ public/js/indexApp.js
@@ -94,15 +94,28 @@ indexApp.controller('mapCtrl', ['$http', '$scope', '$location', 'NgMap', functio
 	                
 	                $scope.heladerias = response.data;
 	                actual = $scope.heladerias[0];
+                  if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                      var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                      };
+                      //infoWindow.setPosition(pos);
+                      //infoWindow.setContent('Location found.');
+                      vm.center=pos;
+                    }, function() {
+                      console.log("Geolocation ok")
+                    });
+                  } else {
+                    // Browser doesn't support Geolocation
+                    alert("Error geolocation");
+                  }
 
 	                /*geolocation.getLocation().then(function(data){
 	            	
 	            		coords = {lat:data.coords.latitude, long:data.coords.longitude};
-	          		});*/
-	          
-	          		//$scope.actual.position=coords;
-	          		$scope.markers=response.data; 
-	                //$scope.$apply();           
+	          		});*/	          
+	          		$scope.markers=response.data;            
 	        }, function errorCallback(response) {
 	                  // called asynchronously if an error occurs
 	                  // or server returns response with an error status.
@@ -175,18 +188,69 @@ indexApp.controller('mapCtrl', ['$http', '$scope', '$location', 'NgMap', functio
 
   $scope.ctrlChanged = function() {
 
-  	var out=[];
+    var out=[];
 
     var num=$scope.heladerias.length; 
     console.log("precio "+$scope.formData.distancia);   
     
-    for(var i=0; i<num; i++){
+    if($scope.formData.distancia != undefined){
+      
+      var distances=[];
+    if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                      var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                      };
+                      
+                      vm.center=pos;
+            queryBody = {
+            longitude: longitude,
+            latitude: latitude,
+            distance: $scope.formData.distance            
+          };
+          $http.post('/query', queryBody)
+          // Store the filtered results in queryResults
+            .success(function(queryResults){
+              distances=queryResults.data;
+              console.log("QueryResults:");
+              console.log(queryResults);
+
+            // Count the number of records retrieved for the panel-footer
+              $scope.queryCount = queryResults.length;
+            })
+            .error(function(queryResults){
+              console.log('Error ' + queryResults);
+            });
+          
+                    }, function() {
+                      console.log("Geolocation ok")
+                    });
+                  } else {
+                    // Browser doesn't support Geolocation
+                    alert("Error geolocation");
+                  }
+    
+      //control de otros filtros a partir de los resultados de distancia
+
+      for(var i=0; i<distances.length; i++){
+        if(($scope.formData.nombre == undefined) || ($scope.formData.nombre != undefined && distances[i].nombre.substring(0,$scope.formData.nombre.length)==$scope.formData.nombre))        
+          if(($scope.formData.delivery==true && distances[i].delivery==true) || $scope.formData.delivery==undefined)
+            if(( $scope.formData.tipohelado=='artesanal' && distances[i].artesanal==true) || ($scope.formData.tipohelado=='artesanal' && distances[i].artesanal==false) || $scope.formData.tipohelado==undefined)
+              if(($scope.formData.precio == undefined) || (distances[i].precio <= $scope.formData.precio))              
+                out.push(distances[i]);
+      }
+    }
+    else{
+      for(var i=0; i<num; i++){
       if(($scope.formData.nombre == undefined) || ($scope.formData.nombre != undefined && $scope.heladerias[i].nombre.substring(0,$scope.formData.nombre.length)==$scope.formData.nombre))        
         if(($scope.formData.delivery==true && $scope.heladerias[i].delivery==true) || $scope.formData.delivery==undefined)
           if(( $scope.formData.tipohelado=='artesanal' && $scope.heladerias[i].artesanal==true) || ($scope.formData.tipohelado=='artesanal' && $scope.heladerias[i].artesanal==false) || $scope.formData.tipohelado==undefined)
             if(($scope.formData.precio == undefined) || ($scope.heladerias[i].precio <= $scope.formData.precio))              
               out.push($scope.heladerias[i]);              
-    }
+      }
+    }    
+
     $scope.markers=out;
     console.log($scope.markers);
   }
